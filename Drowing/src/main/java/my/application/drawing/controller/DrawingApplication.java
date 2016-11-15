0d683fb29a11a583dcd7a228cc5fefdb43a2b3ad
samira001 <samiragj@gmail.com>
2016-11-15 11:38:34 +0530

    sample java application

---
+++ Drowing/src/main/java/my/application/drawing/controller/DrawingApplication.java
@@ -0,0 +1,14 @@
+package my.application.drawing.controller;
+
+import my.application.drawing.service.ShapesForDrowing;
+/**
+ * Created by samira on 11/14 /16.
+ */
+public class DrawingApplication {
+
+    public static void main(String args[]){
+        ShapesForDrowing shapesForDrowing=new ShapesForDrowing();
+        shapesForDrowing.drowTriangular();
+
+    }
+}
---
+++ Drowing/src/main/java/my/application/drawing/service/ShapesForDrowing.java
@@ -0,0 +1,13 @@
+package my.application.drawing.service;
+
+/**
+ * Created by samira on 11/15/16.
+ */
+public class ShapesForDrowing {
+
+    private String triagular;
+
+    public void drowTriangular(){
+        System.out.println("Triangualr has drowed!");
+    }
+}
